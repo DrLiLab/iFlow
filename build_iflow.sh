5c629e1d09fceed6bc7519f94d48107acf54f429
ZhishengZeng <879849680@qq.com>
2021-04-12 02:11:59 +0000

    add build_iflow.sh

---
+++ build_iflow.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd ~
+sudo apt install build-essential clang libreadline6-dev bison flex libffi-dev cmake libboost-all-dev swig klayout libeigen3-dev -y
+
+cd ~
+sudo apt install tcl-dev -y
+sudo cp /usr/include/tcl8.6/*.h /usr/include/
+sudo ln -s /usr/lib/x86_64-linux-gnu/libtcl8.6.so /usr/lib/x86_64-linux-gnu/libtcl8.5.so
+
+cd ~
+wget http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz
+tar zxvf lemon-1.3.1.tar.gz
+cd lemon-1.3.1
+mkdir build && cd build && cmake .. && make && sudo make install
+
+cd ~
+sudo rm -rf lemon-1.3.1 lemon-1.3.1.tar.gz
+
+git clone https://github.com/The-OpenROAD-Project/yosys.git tools/yosys4be891e8
+cd tools/yosys4be891e8
+git checkout 4be891e8
+mkdir build && cd build
+make -f ../Makefile
+cd ../../
+
+git clone https://github.com/The-OpenROAD-Project/TritonRoute.git tools/TritonRoute758cdac
+cd tools/TritonRoute758cdac
+git checkout 758cdac
+mkdir build && cd build 
+cmake .. && make
+cd ../../../../
+
+exit 0
+
+git clone https://github.com/The-OpenROAD-Project/OpenROAD.git tools/OpenROAD9295a533 
+cd tools/OpenROAD9295a533 
+git checkout 9295a533 
+git submodule update --init --recursive
+mkdir build && cd build 
+cmake .. && make
+cd ../../../
