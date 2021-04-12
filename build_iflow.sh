aba807563dac6acb02d74fe104ffba848984cac4
ChibiMarukoZ <40481052+ZhishengZeng@users.noreply.github.com>
2021-04-12 10:49:35 +0800

    Update build_iflow.sh

--- build_iflow.sh
+++ build_iflow.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-cd ~
+# essential package
 sudo apt install build-essential clang libreadline6-dev bison flex libffi-dev cmake libboost-all-dev swig klayout libeigen3-dev -y
 
-cd ~
+# tcl
 sudo apt install tcl-dev -y
 sudo cp /usr/include/tcl8.6/*.h /usr/include/
 sudo ln -s /usr/lib/x86_64-linux-gnu/libtcl8.6.so /usr/lib/x86_64-linux-gnu/libtcl8.5.so
 
-cd ~
+# lemon
 wget http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz
 tar zxvf lemon-1.3.1.tar.gz
 cd lemon-1.3.1
 mkdir build && cd build && cmake .. && make && sudo make install
-
-cd ~
 sudo rm -rf lemon-1.3.1 lemon-1.3.1.tar.gz
 
+# yosys
 git clone https://github.com/The-OpenROAD-Project/yosys.git tools/yosys4be891e8
 cd tools/yosys4be891e8
 git checkout 4be891e8
@@ -24,6 +23,7 @@ mkdir build && cd build
 make -f ../Makefile
 cd ../../
 
+# TritonRoute
 git clone https://github.com/The-OpenROAD-Project/TritonRoute.git tools/TritonRoute758cdac
 cd tools/TritonRoute758cdac
 git checkout 758cdac
@@ -32,7 +32,7 @@ cmake .. && make
 cd ../../../../
 
 exit 0
-
+# OpenROAD
 git clone https://github.com/The-OpenROAD-Project/OpenROAD.git tools/OpenROAD9295a533 
 cd tools/OpenROAD9295a533 
 git checkout 9295a533 
