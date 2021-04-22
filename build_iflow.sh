4f869f5afd4762e89a932070572b881c5979dfeb
ChibiMarukoZ <40481052+ZhishengZeng@users.noreply.github.com>
2021-04-22 11:32:20 +0800

    Update build_iflow.sh

--- build_iflow.sh
+++ build_iflow.sh
@@ -24,7 +24,7 @@ function CHECK_EXIST()
 {
     if [ -d $* ]; then
         # exist
-        echo "[iFlow Warning] dir exist: '$*' skiping..."
+        echo "[iFlow Info] dir exist: '$*' skiping..."
         return 0
     else
         return 1
